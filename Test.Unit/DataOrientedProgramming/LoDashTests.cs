ae3fd992b9d9b68da4e39b7c01f099a7e3fb88bc
phasetr <phasetr@gmail.com>
2024-02-08 17:33:55 +0900

    commit

--- Test.Unit/DataOrientedProgramming/LoDashTests.cs
+++ Test.Unit/DataOrientedProgramming/LoDashTests.cs
@@ -8,23 +8,23 @@ public class LoDashTests
     [Fact]
     public void LoDash_Example1()
     {
-        var map = DataModel.Watchmen;
+        var watchmen = DataModel.Watchmen;
 
-        Assert.Equal("Interesting", _.Get(map, "reviews", "reader1"));
-        Assert.Equal("5 of 5!", _.Get(map, "reviews", "me"));
+        Assert.Equal("Interesting", _.Get(watchmen, "reviews", "reader1"));
+        Assert.Equal("5 of 5!", _.Get(watchmen, "reviews", "me"));
 
-        Assert.Equal("978-1779501127", _.Get(map, "isbn"));
-        Assert.Equal("Watchmen", _.Get(map, "title"));
-        Assert.Equal(1987, _.Get(map, "publicationYear"));
+        Assert.Equal("978-1779501127", _.Get(watchmen, "isbn"));
+        Assert.Equal("Watchmen", _.Get(watchmen, "title"));
+        Assert.Equal(1987, _.Get(watchmen, "publicationYear"));
 
-        Assert.Equal("alan-moore", _.Get(map, ["authors", "0"]));
-        Assert.Equal("dave-gibbons", _.Get(map, ["authors", "1"]));
+        Assert.Equal("alan-moore", _.Get(watchmen, ["authors", "0"]));
+        Assert.Equal("dave-gibbons", _.Get(watchmen, ["authors", "1"]));
 
-        Assert.Equal("book-item-1", _.Get(map, ["bookItems", "0", "id"]));
-        Assert.Equal("nyc-central-lib", _.Get(map, ["bookItems", "0", "libId"]));
-        Assert.True((bool) _.Get(map, ["bookItems", "0", "isLent"]));
-        Assert.Equal("book-item-2", _.Get(map, ["bookItems", "1", "id"]));
-        Assert.Equal("nyc-central-lib", _.Get(map, ["bookItems", "1", "libId"]));
-        Assert.False((bool) _.Get(map, ["bookItems", "1", "isLent"]));
+        Assert.Equal("book-item-1", _.Get(watchmen, ["bookItems", "0", "id"]));
+        Assert.Equal("nyc-central-lib", _.Get(watchmen, ["bookItems", "0", "libId"]));
+        Assert.True((bool) _.Get(watchmen, ["bookItems", "0", "isLent"]));
+        Assert.Equal("book-item-2", _.Get(watchmen, ["bookItems", "1", "id"]));
+        Assert.Equal("nyc-central-lib", _.Get(watchmen, ["bookItems", "1", "libId"]));
+        Assert.False((bool) _.Get(watchmen, ["bookItems", "1", "isLent"]));
     }
 }
