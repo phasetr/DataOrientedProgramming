85fd5ac79cb33af1fc45f8e50894ac45ce8126cd
phasetr <phasetr@gmail.com>
2024-02-15 14:06:51 +0900

    commit

--- BookSample/Functions/UserManagement.cs
+++ BookSample/Functions/UserManagement.cs
@@ -5,9 +5,37 @@ namespace BookSample.Functions;
 
 public static class UserManagement
 {
+    /// <summary>
+    /// P.79, リスト3-20
+    /// </summary>
+    /// <param name="userManagementData"></param>
+    /// <param name="email"></param>
+    /// <returns></returns>
     public static bool IsLibrarian(ImmutableDictionary<string, dynamic> userManagementData, string email)
     {
         var dict = (ImmutableDictionary<string, dynamic>) _.Get(userManagementData, "librariansByEmail", email);
         return dict.ContainsValue(email);
+    }
+
+    /// <summary>
+    /// P.80, リスト3-30
+    /// </summary>
+    /// <param name="userManagementData"></param>
+    /// <param name="email"></param>
+    /// <returns></returns>
+    public static bool IsVipMember(ImmutableDictionary<string,dynamic> userManagementData, string email)
+    {
+        return (bool) _.Get(userManagementData, "membersByEmail", email, "isVIP");
+    }
+
+    /// <summary>
+    /// P.80, リスト3-30
+    /// </summary>
+    /// <param name="userManagementData"></param>
+    /// <param name="email"></param>
+    /// <returns></returns>
+    public static bool IsSuperMember(ImmutableDictionary<string,dynamic> userManagementData, string email)
+    {
+        return (bool) _.Get(userManagementData, "membersByEmail", email, "isSuper");
     }
 }
